3c53f019c9041e2037484a55f46f902aec9618f4
GensengG <168531024+GensengG@users.noreply.github.com>
2024-10-04 23:01:32 +0300

    Update task.js

---
+++ js-features/countdown/task.js
@@ -0,0 +1,31 @@
+let timerElement = document.getElementById("timer");
+let startValue = Number(timerElement.textContent);
+function timer() {
+    if(startValue > 0){
+        startValue -=1;
+        timerElement.textContent = startValue;
+    } else {
+    alert ("Вы победили в конкурсе!");
+    }
+}
+
+timer();
+setInterval(timer, 1000)
+
+
+// Повышенный уровень сложности #1
+
+let timerElement = document.getElementById("timer");
+timerElement.textContent = "00:00:30";
+let startValueArr = Array.from(timerElement.textContent);
+let startValue = Number(startValueArr[6] + startValueArr[7]);
+function timer() {
+    if(startValue > 0){
+        startValue -=1;
+        timerElement.textContent = `00:00:${startValue}`;
+    } else {
+    alert ("Вы победили в конкурсе!");
+    }
+}
+timer();
+setInterval(timer, 1000)
