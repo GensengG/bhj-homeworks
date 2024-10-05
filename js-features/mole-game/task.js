dfcb99f24bbf910771eeecac6d2584e9b5548425
GensengG <168531024+GensengG@users.noreply.github.com>
2024-10-05 17:06:34 +0300

    Update task.js

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -3,32 +3,47 @@ let deadCount = Number(dead.textContent);
 let lost = document.getElementById("lost");
 let lostCount = Number(lost.textContent);
 
-function click() {
-    getHole = index => document.getElementById(`hole${index}`);
-    for(let holeItem = 1; holeItem < 10; holeItem++){
-        let holeClick = getHole(holeItem);
-        holeClick.addEventListener("click", function() {
-            if(holeClick.className === "hole hole_has-mole"){
-                deadCount ++;
-                dead.textContent = deadCount;
-            } else {
-                lostCount ++;
-                lost.textContent = lostCount;
-            }
-        })
-    }
-}
-
 function game() {
-    if(lostCount === 5 || deadCount === 10){
-        deadCount = 0;
-        dead.textContent = deadCount;
-        lostCount = 0;
-        lost.textContent = lostCount;
-        return;
-    } 
+    function check(){
+        if(lostCount === 5){
+            deadCount = 0;
+            dead.textContent = deadCount;
+            lostCount = 0;
+            lost.textContent = lostCount;
+            alert ("Вы проиграли(")
+            return;
+        } 
+
+        if(deadCount === 10){
+            deadCount = 0;
+            dead.textContent = deadCount;
+            lostCount = 0;
+            lost.textContent = lostCount;
+            alert ("Вы выиграли!")
+            return;
+        } 
+
+    }
+
+    function click() {
+        getHole = index => document.getElementById(`hole${index}`);
+        for(let holeItem = 1; holeItem < 10; holeItem++){
+            let holeClick = getHole(holeItem);
+            holeClick.addEventListener("click", function() {
+                if(holeClick.className === "hole hole_has-mole"){
+                    deadCount ++;
+                    dead.textContent = deadCount;
+                } else {
+                    lostCount ++;
+                    lost.textContent = lostCount;
+                }
+                
+                check();
+            })
+        }
+    }
+    
     click();
 }
-
 
 game();
