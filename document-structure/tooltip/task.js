97a82385fe0feecf96dc4e4e2b7cd189783ad216
GensengG <168531024+GensengG@users.noreply.github.com>
2024-10-22 22:53:15 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,7 +4,7 @@ tooltipArr.forEach((elem) => {
     let createTooltip = document.createElement("div");
     createTooltip.textContent = String(elem.title);
     createTooltip.classList.add("tooltip");
-    elem.appendChild(createTooltip);
+    elem.insertAdjacentElement("afterEnd", createTooltip);
     const elemCoordinates = elem.getBoundingClientRect();
     let left = Math.round(elemCoordinates.left);
     elem.addEventListener("click", (e) => {
