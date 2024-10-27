e31cb224111fccc5fe8043def61e65df202ef81e
GensengG <168531024+GensengG@users.noreply.github.com>
2024-10-27 18:26:31 +0300

    Update task.js

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -7,23 +7,23 @@ tasksBtn.addEventListener("click", (e) => {
     e.preventDefault();
     if(String(tasksInput.value).trim() === ""){
         tasksInput.value = null;
-    } else {
-        tasksList.insertAdjacentHTML("afterbegin", `
-            <div class="task">
-              <div class="task__title">
-                ${tasksInput.value};
-              </div>
-              <a href="#" class="task__remove">
-                &times;
-              </a>
-            </div>
-        `);
-        tasksInput.value = null;
+        return;
+    } 
+    tasksList.insertAdjacentHTML("afterbegin", `
+        <div class="task">
+          <div class="task__title">
+            ${tasksInput.value}
+          </div>
+          <a href="#" class="task__remove">
+            &times;
+          </a>
+        </div>
+    `);
+    tasksInput.value = null;
 
-        let taskRemove = document.querySelector(".task__remove");
-        taskRemove.addEventListener("click", () => {
-            let parentElem = taskRemove.parentElement;
-            parentElem.outerHTML = "";
-        })
-    }
+    let taskRemove = document.querySelector(".task__remove");
+      taskRemove.addEventListener("click", () => {
+          let parentElem = taskRemove.parentElement;
+          parentElem.outerHTML = "";
+      })
 })
